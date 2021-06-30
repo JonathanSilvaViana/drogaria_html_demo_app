907eb1fd28b492e106a6c7bcb0bbdaa5f73d5cc4
JonathanSilvaViana <jonathansilva@daum.net>
2021-06-30 08:40:22 -0300

    web view completa

--- app/src/main/java/br/com/drogariaappdemonstracao/NavegadorLocalActivity.java
+++ app/src/main/java/br/com/drogariaappdemonstracao/NavegadorLocalActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -18,12 +19,20 @@ public class NavegadorLocalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_navegador_local);
         init();
 
+      //  webview.setJavaScriptEnabled(true);
+
 
     }
 
     private void init(){
         webview = (WebView)findViewById(R.id.webview);
-        webview.loadUrl("file:///android_asset/download.html");
+        webview.loadUrl("file:///android_asset/index.html");
+        webview.setHapticFeedbackEnabled(true);
+        //permite o botão de retorno
+        webview.canGoBack();
+        //permite alterações com javascript habilitado
+        webview.getSettings().setJavaScriptEnabled(true);
+
         webview.requestFocus();
 
         progressDialog = new ProgressDialog(NavegadorLocalActivity.this);
@@ -41,5 +50,22 @@ public class NavegadorLocalActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webview.canGoBack()) {
+                        webview.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
