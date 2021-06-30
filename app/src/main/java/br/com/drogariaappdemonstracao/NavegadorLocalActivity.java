c476439dc2cb3bf2745d642e2d0bd9c64821bfc6
JonathanSilvaViana <jonathansilva@daum.net>
2021-06-29 21:42:05 -0300

    web view local funcional

--- app/src/main/java/br/com/drogariaappdemonstracao/NavegadorLocalActivity.java
+++ app/src/main/java/br/com/drogariaappdemonstracao/NavegadorLocalActivity.java
@@ -2,13 +2,44 @@ package br.com.drogariaappdemonstracao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class NavegadorLocalActivity extends AppCompatActivity {
+
+    WebView webview;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navegador_local);
+        init();
+
+
+    }
+
+    private void init(){
+        webview = (WebView)findViewById(R.id.webview);
+        webview.loadUrl("file:///android_asset/download.html");
+        webview.requestFocus();
+
+        progressDialog = new ProgressDialog(NavegadorLocalActivity.this);
+        progressDialog.setMessage(getString(R.string.loading));
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        webview.setWebViewClient(new WebViewClient() {
+
+            public void onPageFinished(WebView view, String url) {
+                try {
+                    progressDialog.dismiss();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
